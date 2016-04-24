16b71b3186b0388cdbf38cad9e935a91789c5b93
sreekumar456 <ragemusker@gmail.com>
2016-04-25 01:24:59 +0530

    Sree:Adding JQuery Library

--- src/main/webapp/scripts/common/controllers/homeCtrl.js
+++ src/main/webapp/scripts/common/controllers/homeCtrl.js
@@ -1,7 +1,8 @@
 
-controllers.controller('HomeController',['$scope','$log',function($scope,$log){
+controllers.controller('HomeController',['$scope',
+    function($scope){
     $scope.myInterval = 5000;
     $scope.travissVersion = "";
-    var slides = $scope.slides = [{image:'images/Gantry.jpg',text: 'Gantry'},{image:'images/Toll.jpg',text:'Toll Point'}];   
+//    var slides = $scope.slides = [{image:'images/Gantry.jpg',text: 'Gantry'},{image:'images/Toll.jpg',text:'Toll Point'}];   
    
 }]);
